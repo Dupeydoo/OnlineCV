3d7248cb5e3a3b78d78da03665319b13ed8ca2a5
Dupeydoo <jrmd202@exeter.ac.uk>
2017-02-15 13:00:01 +0000

    Refactored and encapuslated JS code, more responsiveness changes at low screen sizes

--- js/page.js
+++ js/page.js
@@ -1,12 +1,12 @@
-///Smooth Scrolling
+////////////////Smooth Scrolling////////////////
+
 
 /*
 	Binds the smooth scroll to the click event. When the user clicks a link which uses the # to reference
 	an element it should be scrolled to non-abruptly.
 */
 $("a[href*='#']:not([href='#'])").click(function() {
-	//if the two respective urls have the same hostname and normal path
-	if(location.pathname.replace(/^.*\/([^/]*)/, '') == this.pathname.replace(/^.*\/([^/]*)/, '') && location.hostname == this.hostname) {
+	if(location.pathname.replace(/^.*\/([^/]*)/, '') == this.pathname.replace(/^.*\/([^/]*)/, '') && location.hostname == this.hostname) { //if the two respective urls have the same hostname and normal path
 		var scrollTo =  $(this.hash);   //get the whole id value, e.g. #foo
 
 		if(scrollTo.length) {
@@ -24,6 +24,9 @@ $("a[href*='#']:not([href='#'])").click(function() {
 
 /*
 	Actually performs the smooth scroll through an animation setting the page scroll.
+
+	@param: the target to scroll to
+	@return: false if scrollTo does not have a length
 */
 function scroll(scrollTo){
 	if(scrollTo.length) {
@@ -34,13 +37,31 @@ function scroll(scrollTo){
 	return false;
 }
 
-///Responsive code
+
+////////////////Responsive code////////////////
+
 
 /*
-	Use the window resize event to see if the width is less than 600, if so add classes to change the buttons.
+	Needs to be checked if page is reloaded for responsive buttons.
+*/
+$(document).ready(function() {
+	changeButton(600);
+})
+
+/*
+	Use the window resize event to see if the width is less than 600, if so change the buttons.
 */
 $(window).on('resize', function() {
-	if($(window).width() < 600) {
+	changeButton(600);
+})
+
+/*
+	Does the actual legwork in checking and managing the button classes.
+
+	@param: the width where the buttons should change
+*/
+function changeButton(width){
+	if($(window).width() < width) {
 		$('.wrap-main .main-section .button-group a').removeClass('button-pill').addClass('button-square');
 		$('.wrap-main .main-section #menu-button .button-dropdown button').removeClass('button-large').addClass('button-square')
 	}
@@ -49,4 +70,4 @@ $(window).on('resize', function() {
 		$('.wrap-main .main-section .button-group a').removeClass('button-square').addClass('button-pill');
 		$('.wrap-main .main-section #menu-button .button-dropdown button').removeClass('button-square').addClass('button-large')
 	}
-})
+}
